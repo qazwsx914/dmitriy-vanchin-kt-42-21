110412b29697d818334628dcfda3066545237740
vanchin-dmitriy-kt-42-21 <dimas.egorov1899@gmail.com>
2024-09-11 14:23:50 +0300

    Пустой проект на ноутбуке

--- dmitriyVanchinKt-42-21/Controllers/WeatherForecastController.cs
+++ dmitriyVanchinKt-42-21/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace dmitriyVanchinKt_42_21.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
